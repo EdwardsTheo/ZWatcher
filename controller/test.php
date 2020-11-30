7b22585da415de68ed8db30b857792fea7ddee62
Thomas Paris <thomasparis56@gmail.com>
2020-11-30 15:05:13 +0100

    test2 ssh

--- controller/test.php
+++ controller/test.php
@@ -1,4 +1,7 @@
-<?php $connection = ssh2_connect('82.64.225.10', 2020);
-ssh2_auth_password($connection, 'barney', 'stinson');
-$stream = ssh2_exec($connection, 'mkdir /home/barney/test');
+<html>
+<?php 
+    $connection = ssh2_connect('82.64.225.10', 2020);
+    ssh2_auth_password($connection, 'barney', 'stinson');
+    $stream = ssh2_exec($connection, 'mkdir /home/barney/test');
 ?>
+</html>
