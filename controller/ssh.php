5ee962a952646099cd0a57a2412e70049bba7be4
root <baptiste.theobald@outlook.fr>
2020-12-01 14:28:30 +0100

    test_php_exec

---
+++ controller/ssh.php
@@ -0,0 +1,11 @@
+<?php
+
+$con = ssh2_connect("82.64.225.10", 2020);
+ssh2_auth_password($con, 'barney', 'stinson'); 
+$stream = ssh2_exec($con, 'ls -la');
+stream_set_blocking($stream, true);
+  $output = stream_get_contents($stream);
+  echo nl2br ("$output \n");
+  
+  unset($con);
+?>
