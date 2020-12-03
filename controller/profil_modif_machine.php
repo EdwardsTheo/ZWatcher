88fa773acf8c7786c957266be72e57fc2e1ad6e2
Thomas Paris <thomasparis56@gmail.com>
2020-12-03 11:42:27 +0100

    modif pages options

--- controller/profil_modif_machine.php
+++ controller/profil_modif_machine.php
@@ -1,7 +1,10 @@
 <?php
 
-    $machine_name = $_POST['machine_name'];
+    $machine_name = $_POST['id'];
     $_SESSION['machine_name'] = $machine_name;
+    $_SESSION['errors'] = "";
+    $_SESSION['errors_2'] = "";
+    $_SESSION['errors_3'] = "";
 
     require('../view/profil_views/begin_modif.php');
 
