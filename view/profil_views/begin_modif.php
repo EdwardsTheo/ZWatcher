4b5d4c7eea90cc0664aabe48ca9ed7e98f0c95b3
EdwardsTheo <baptiste.theobald@outlook.fr>
2020-12-17 11:06:57 +0100

    user_groups

--- view/profil_views/begin_modif.php
+++ view/profil_views/begin_modif.php
@@ -8,6 +8,8 @@
                     <a href="?action=modif_liste"><button class="w3-button w3-white"><i class="fas fa-info w3-margin-right"></i>Informations</button></a>
                     <a href="#"><button class="w3-button w3-black"><i class="fas fa-globe-asia w3-margin-right"></i>Profil</button></a>
                     <a href="#"><button class="w3-button w3-white w3-hide-small"><i class="fas fa-tools w3-margin-right"></i>Paramètres</button></a>
+                    <a href="?action=modif_users"><button class="w3-button w3-white w3-hide-small"><i class="fas fa-tools w3-margin-right"></i>Gérer les users</button></a>
+                    <a href="?action=modif_groups"><button class="w3-button w3-white w3-hide-small"><i class="fas fa-tools w3-margin-right"></i>Gérer les groupes</button></a>
                     </div>
                     </div>
                     </div>
