4f815a6d91a7457d897814aa175a3c1095270bea
Thomas Paris <thomasparis56@gmail.com>
2020-12-20 05:53:46 +0100

    mail calling 1.1

--- controller/mail/confirmation_account.php
+++ controller/mail/confirmation_account.php
@@ -1,12 +1,12 @@
 <?php
-                $to      = 'thomasparis56@gmail.com';
-                $subject = 'test Mail';
-                $message = 'Salut je teste des trucs';
-                $headers = 'From: "ZWatcher"<noreply@zwatcher.com>' . "\r\n" .
-                'Reply-To: noreply@zwatcher.com' . "\r\n" .
-                'X-Mailer: PHP/' . phpversion();
+    $to      = 'thomasparis56@gmail.com';
+    $subject = 'test Mail';
+    $message = 'Salut je teste des trucs';
+    $headers = 'From: "ZWatcher"<noreply@zwatcher.com>' . "\r\n" .
+    'Reply-To: noreply@zwatcher.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
 
-                mail($to, $subject, $message, $headers);
+    mail($to, $subject, $message, $headers);
 
-                echo(error_get_last()['message']);
-                ?> 
+    echo(error_get_last()['message']);
+?> 
--- controller/send_confirmation_account.php
+++ controller/send_confirmation_account.php
@@ -2,16 +2,7 @@
 
     session_start();
 
-    $to = 'thomasparis56@gmail.com';
-    $subject = 'test Mail';
-    $message = 'Salut je teste des trucs';
-    $headers = 'From: "ZWatcher"<noreply@zwatcher.com>' . "\r\n" .
-    'Reply-To: noreply@zwatcher.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-
-    mail($to, $subject, $message, $headers);
-
-    echo(error_get_last()['message']);
+    include('./mail/confirmation_account.php');
 
     
     header('location: ../view/profil.php?action=status');
