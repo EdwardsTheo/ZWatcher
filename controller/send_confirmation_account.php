e0c13b69ea6ef07fc1f68dd8063b95c38e82a1cd
Thomas Paris <thomasparis56@gmail.com>
2020-12-20 07:00:56 +0100

    mail rebuild 1.2

--- controller/send_confirmation_account.php
+++ controller/send_confirmation_account.php
@@ -2,7 +2,16 @@
 
     session_start();
 
-    include('./mail/confirmation_account.php');
+    $to      = 'thomasparis56@gmail.com';
+    $subject = 'test Mail';
+    $message = 'Salut je teste des trucs';
+    $headers = 'From: "ZWatcher"<noreply@zwatcher.com>' . "\r\n" .
+    'Reply-To: noreply@zwatcher.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+    mail($to, $subject, $message, $headers);
+
+    echo(error_get_last()['message']);
 
     
     header('location: ../view/profil.php?action=status');
