7402e90ae0764c29d787693aac59ff54652add20
Thomas Paris <thomasparis56@gmail.com>
2020-12-30 06:22:40 +0100

    begin prevent url direct access

--- view/connect_view.php
+++ view/connect_view.php
@@ -2,6 +2,14 @@
 
 //session_start();
 
+if ( $_SERVER['REQUEST_METHOD']!=='POST' && realpath(__FILE__) == realpath( $_SERVER['SCRIPT_FILENAME'] ) ) {
+ 
+    header( 'HTTP/1.0 403 Forbidden', TRUE, 403 );
+
+    die( header( 'location: ../view/error.php' ) );
+
+}
+
 ?>
 
 <!DOCTYPE HTML>
---
+++ view/error.php
@@ -0,0 +1,40 @@
+<?php
+
+//session_start();
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+    <head>
+		    <title>ZWatcher</title>
+		    <meta charset="utf-8" />
+        <link rel="stylesheet" href="../assets/css/connect_view.css" />
+        <script src="https://kit.fontawesome.com/44bec37701.js"></script>
+	  </head>
+	  <body>
+
+        <div class="brand">
+            <img src="../images/graphismes/logo_transparent_centre.png"/>
+            </a>
+        </div>
+
+        <div class="brand">
+            <img src="../images/graphismes/logo_transparent_centre.png"/>
+            </a>
+        </div>
+
+        <div class="login">
+            <div class="login_title">
+                <span>La page demandée n'est pas accessible</span>
+            </div>
+
+            <div class="disclaimer">
+                <center>ZWatcher 2020-2021. </center> </br>
+                <center>Tous droits réservés - All rights reserved. </center>
+            </div>
+        </div>
+
+
+    </body>
+</html>
